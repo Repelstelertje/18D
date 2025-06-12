7e5fc4481e137039536a964365e747418808528a
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-12 12:49:23 +0200

    Remove unused navItems3 block and clean error log

--- includes/nav.php
+++ includes/nav.php
@@ -23,16 +23,4 @@
 		  	?>
 		</div>
 	</li>
-	<!--<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Datingtips</a>
-		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		  	<?php
-
-		    	foreach ($navItems3 as $item3) {
-		      		echo "<a class=\"dropdown-item\" href=\"$item3[slug]\">$item3[title]</a>";
-		    	}
-		    
-		  	?>
-		</div>
-	</li>-->
 </ul>
