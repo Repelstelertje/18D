48fe2803f3f5d0020b5cd337e590442a5f320081
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-12 12:40:12 +0200

    Update nav_items.php

--- includes/nav_items.php
+++ includes/nav_items.php
@@ -3,100 +3,100 @@
 	$navItems = array(
 
 					array(
-						slug 	=> 'sexdate-drenthe',
-						title 	=> 'Drenthe'
+						'slug' 	=> 'sexdate-drenthe',
+						'title' 	=> 'Drenthe'
 					),
 					array(
-						slug 	=> 'sexdate-flevoland',
-						title 	=> 'Flevoland'
+						'slug' 	=> 'sexdate-flevoland',
+						'title' 	=> 'Flevoland'
 					),
 					array(
-						slug 	=> 'sexdate-friesland',
-						title 	=> 'Friesland'
+						'slug' 	=> 'sexdate-friesland',
+						'title' 	=> 'Friesland'
 					),
 					array(
-						slug 	=> 'sexdate-gelderland',
-						title 	=> 'Gelderland'
+						'slug' 	=> 'sexdate-gelderland',
+						'title' 	=> 'Gelderland'
 					),
 					array(
-						slug 	=> 'sexdate-groningen',
-						title 	=> 'Groningen'
+						'slug' 	=> 'sexdate-groningen',
+						'title' 	=> 'Groningen'
 					),
 					array(
-						slug 	=> 'sexdate-limburg',
-						title 	=> 'Limburg'
+						'slug' 	=> 'sexdate-limburg',
+						'title' 	=> 'Limburg'
 					),
 					array(
-						slug 	=> 'sexdate-noord-brabant',
-						title 	=> 'Noord-Brabant'
+						'slug' 	=> 'sexdate-noord-brabant',
+						'title' 	=> 'Noord-Brabant'
 					),
 					array(
-						slug 	=> 'sexdate-noord-holland',
-						title 	=> 'Noord-Holland'
+						'slug' 	=> 'sexdate-noord-holland',
+						'title' 	=> 'Noord-Holland'
 					),
 					array(
-						slug 	=> 'sexdate-overijssel',
-						title 	=> 'Overijssel'
+						'slug' 	=> 'sexdate-overijssel',
+						'title' 	=> 'Overijssel'
 					),
 					array(
-						slug 	=> 'sexdate-utrecht',
-						title 	=> 'Utrecht'
+						'slug' 	=> 'sexdate-utrecht',
+						'title' 	=> 'Utrecht'
 					),
 					array(
-						slug 	=> 'sexdate-zeeland',
-						title 	=> 'Zeeland'
+						'slug' 	=> 'sexdate-zeeland',
+						'title' 	=> 'Zeeland'
 					),
 					array(
-						slug 	=> 'sexdate-zuid-holland',
-						title 	=> 'Zuid-Holland'
+						'slug' 	=> 'sexdate-zuid-holland',
+						'title' 	=> 'Zuid-Holland'
 					),
 				);
 
 	$navItems2 = array(
 
 					array(
-						slug 	=> 'sexdates-antwerpen',
-						title 	=> 'Antwerpen'
+						'slug' 	=> 'sexdates-antwerpen',
+						'title' 	=> 'Antwerpen'
 					),
 					array(
-						slug 	=> 'sexdates-brussel',
-						title 	=> 'Brussel'
+						'slug' 	=> 'sexdates-brussel',
+						'title' 	=> 'Brussel'
 					),
 					array(
-						slug 	=> 'sexdates-henegouwen',
-						title 	=> 'Henegouwen'
+						'slug' 	=> 'sexdates-henegouwen',
+						'title' 	=> 'Henegouwen'
 					),
 					array(
-						slug 	=> 'sexdates-limburg',
-						title 	=> 'Limburg'
+						'slug' 	=> 'sexdates-limburg',
+						'title' 	=> 'Limburg'
 					),
 					array(
-						slug 	=> 'sexdates-luik',
-						title 	=> 'Luik'
+						'slug' 	=> 'sexdates-luik',
+						'title' 	=> 'Luik'
 					),
 					array(
-						slug 	=> 'sexdates-luxemburg',
-						title 	=> 'Luxemburg'
+						'slug' 	=> 'sexdates-luxemburg',
+						'title' 	=> 'Luxemburg'
 					),
 					array(
-						slug 	=> 'sexdates-namen',
-						title 	=> 'Namen'
+						'slug' 	=> 'sexdates-namen',
+						'title' 	=> 'Namen'
 					),
 					array(
-						slug 	=> 'sexdates-oost-vlaanderen',
-						title 	=> 'Oost-Vlaanderen'
+						'slug' 	=> 'sexdates-oost-vlaanderen',
+						'title' 	=> 'Oost-Vlaanderen'
 					),
 					array(
-						slug 	=> 'sexdates-vlaams-brabant',
-						title 	=> 'Vlaams-Brabant'
+						'slug' 	=> 'sexdates-vlaams-brabant',
+						'title' 	=> 'Vlaams-Brabant'
 					),
 					array(
-						slug 	=> 'sexdates-waals-brabant',
-						title 	=> 'Waals-Brabant'
+						'slug' 	=> 'sexdates-waals-brabant',
+						'title' 	=> 'Waals-Brabant'
 					),
 					array(
-						slug 	=> 'sexdates-west-vlaanderen',
-						title 	=> 'West-Vlaanderen'
+						'slug' 	=> 'sexdates-west-vlaanderen',
+						'title' 	=> 'West-Vlaanderen'
 					),
 				);
 ?>
