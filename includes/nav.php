adbdf0bd7c8c2c9b0d9044c2249ab63a73bbfa75
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-21 10:30:01 +0200

    Update navigation to single Europe dropdown

--- includes/nav.php
+++ includes/nav.php
@@ -1,66 +1,13 @@
 <ul class="navbar-nav ml-auto">
-	<li class="nav-item dropdown"> 
-		<a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in Nederland</a>
-		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		  	<?php
-
-		    	foreach ($navItemsNL as $item) {
-		      		echo "<a class=\"dropdown-item\" href=\"$item[slug]\">$item[title]</a>";
-		    	}
-		    
-		  	?>
-		</div>
-</li>
-<li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in België</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php
-
-                        foreach ($navItemsBE as $item) {
-                                echo "<a class=\"dropdown-item\" href=\"$item[slug]\">$item[title]</a>";
-                        }
-
-                        ?>
-                </div>
-        </li>
         <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in UK</a>
+                <a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in Europa</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php
-                        foreach ($navItemsUK as $item) {
-                                echo "<a class=\"dropdown-item\" href=\"$item[slug]\">$item[title]</a>";
-                        }
-                        ?>
-                </div>
-        </li>
-        <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in Duitsland</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php
-                        foreach ($navItemsDE as $item) {
-                                echo "<a class=\"dropdown-item\" href=\"$item[slug]\">$item[title]</a>";
-                        }
-                        ?>
-                </div>
-        </li>
-        <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in Oostenrijk</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php
-                        foreach ($navItemsAT as $item) {
-                                echo "<a class=\"dropdown-item\" href=\"$item[slug]\">$item[title]</a>";
-                        }
-                        ?>
-                </div>
-        </li>
-        <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle drpdwn" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sexdates in Zwitserland</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php
-                        foreach ($navItemsCH as $item) {
-                                echo "<a class=\"dropdown-item\" href=\"$item[slug]\">$item[title]</a>";
-                        }
-                        ?>
+                        <a class="dropdown-item" href="/index.php#nederland">Nederland</a>
+                        <a class="dropdown-item" href="/index.php#belgie">Belgi&euml;</a>
+                        <a class="dropdown-item" href="/index.php#uk">United Kingdom</a>
+                        <a class="dropdown-item" href="/index.php#duitsland">Duitsland</a>
+                        <a class="dropdown-item" href="/index.php#oostenrijk">Oostenrijk</a>
+                        <a class="dropdown-item" href="/index.php#zwitserland">Zwitserland</a>
                 </div>
         </li>
         <li class="nav-item dropdown">
